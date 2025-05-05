c45e8e669ecb2d94f32ef1cca95654e4de16f72a
Aleksey Goryansky <aleksejgoryansky@yandex.ru>
2025-05-05 16:33:34 +0300

    feat(core/models): добавить класс 'Concert' и создать миграции

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Concert(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Навзвание концерта')
+    date = models.DateField(verbose_name='Дата концерта')
+    city = models.CharField(max_length=100, verbose_name='Город')
+    location = models.CharField(max_length=200, verbose_name='Место проведения')
+    ticket = models.URLField(verbose_name='Ссылка на покупку билетов')
+    meeting = models.URLField(verbose_name='Cсылка на встречу')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Концерт'
+        verbose_name_plural = 'Концерты'
+        ordering = ['-date']
