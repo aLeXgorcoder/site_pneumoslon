a8df1d4a7aea442576f3e13da3e2e7af82eef789
Aleksey Goryansky <aleksejgoryansky@yandex.ru>
2025-05-05 20:45:18 +0300

    feat(core): добавить модель Song в админ-панель

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Concert, Album
+from .models import Concert, Album, Song
 
 admin.site.register(Concert)
 admin.site.register(Album)
+admin.site.register(Song)
